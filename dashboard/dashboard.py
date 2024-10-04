aa664713e10988b0473ac6a58ad3e340b1feeeed
jramadhannw <jelangramadhannw@gmail.com>
2024-10-04 13:56:50 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,20 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load the dataset
 data = pd.read_csv('dashboard/day.csv')
 
-# Set the title of the dashboard
 st.title("Bike Sharing Data Analysis")
 
-# Display dataset information
 st.write("### Dataset Overview")
 st.write(data.head())
 
-# Group data by weather and aggregate rental count
 weatherRental = data.groupby('weathersit')['cnt'].sum().reset_index()
 
-# Visualization for rentals by weather
 st.write("### Bike Rentals by Weather Condition")
 fig, ax = plt.subplots()
 sns.barplot(x='weathersit', y='cnt', data=weatherRental, ax=ax)
@@ -27,10 +22,8 @@ ax.set_xticks([0, 1, 2, 3])
 ax.set_xticklabels(['Clear', 'Mist', 'Light Rain', 'Heavy Rain'])
 st.pyplot(fig)
 
-# Group data by season and aggregate rental count
 seasonRental = data.groupby('season')['cnt'].sum().reset_index()
 
-# Visualization for rentals by season
 st.write("### Bike Rentals by Season")
 fig, ax = plt.subplots()
 sns.barplot(x='season', y='cnt', data=seasonRental, ax=ax)
@@ -41,10 +34,8 @@ ax.set_xticks([0, 1, 2, 3])
 ax.set_xticklabels(['Spring', 'Summer', 'Fall', 'Winter'])
 st.pyplot(fig)
 
-# Correlation Matrix feature
 st.write("### Correlation Matrix of Key Features")
 
-# Generate the correlation matrix
 plt.figure(figsize=(12, 8))
 correlation = data[['temp', 'atemp', 'hum', 'windspeed', 'cnt']].corr()
 
