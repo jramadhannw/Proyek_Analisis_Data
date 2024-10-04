cec00ff8178da24ddd690766e681c6b88f76c6c0
jramadhannw <jelangramadhannw@gmail.com>
2024-10-04 14:05:46 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,20 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load the dataset
 data = pd.read_csv('dashboard/day.csv')
 
-# Set the title for the Streamlit app
 st.title("Analysis of Bike Sharing Data")
 
-# Display an overview of the dataset
 st.write("### Overview of the Dataset")
 st.write(data.head())
 
-# Group the data by weather conditions and calculate total rentals
 weather_rentals = data.groupby('weathersit')['cnt'].sum().reset_index()
 
-# Show bike rentals categorized by weather conditions
 st.write("### Total Bike Rentals by Weather Condition")
 fig, ax = plt.subplots()
 sns.barplot(x='weathersit', y='cnt', data=weather_rentals, ax=ax)
@@ -41,13 +36,11 @@ ax.set_xticks([0, 1, 2, 3])
 ax.set_xticklabels(['Spring', 'Summer', 'Fall', 'Winter'])
 st.pyplot(fig)
 
-# Show the correlation matrix for key features
 st.write("### Correlation Matrix of Important Features")
 
 plt.figure(figsize=(12, 8))
 correlation_matrix = data[['temp', 'atemp', 'hum', 'windspeed', 'cnt']].corr()
 
-# Create and display the heatmap for the correlation matrix
 fig, ax = plt.subplots()
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", ax=ax)
 ax.set_title('Correlation Matrix')
